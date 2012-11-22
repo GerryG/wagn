684c3e3a6ee67721a83575e6923a0f13850eaab1
Gerry Gleason <gerrg@inbox.com>
2012-11-22 00:46:12 -0600

    User names have to be codenames to use a symbol

--- spec/models/card/confirmation_spec.rb
+++ spec/models/card/confirmation_spec.rb
@@ -10,7 +10,7 @@ describe Card, "destroy without dependents" do
 end
 
 describe Card, "destroy with dependents" do
-  before do Account.as(:joe_user); @c = Card["A"] end
+  before do Account.as('joe_user'); @c = Card["A"] end
     
   it "should succeed" do
     #should test for destruction of dependents!

3cf0a630d0997695277debc136f0e4951e321d73
Gerry Gleason <gerrg@inbox.com>
2012-12-11 10:25:06 -0600

    add test and migration skeleton for new code card interface

---
+++ spec/models/card/account_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path('../../spec_helper', File.dirname(__FILE__))
+
+describe Card, "account functions" do
+  before(:each) do
+    Account.as :joe_user
+  end
+
+  it "should signin" do
+  end
+
+  it "should signout" do
+  end
+
+  it "should signup" do
+  end
+
+  it "should accept" do
+  end
+
+  it "should invite" do
+  end
+
+  it "should generate new password on forgotpassword" do
+  end
+
+end

62a4755501bc4eea2b2bea64535567db61222bf9
Gerry Gleason <gerrg@inbox.com>
2012-12-28 15:40:00 -0600

    Mistake in migration update

--- db/migrate/20121118115000_update_link_type.rb
+++ db/migrate/20121118115000_update_link_type.rb
@@ -5,19 +5,19 @@ class UpdateLinkType < ActiveRecord::Migration
   include Card::ReferenceTypes
 
   LINK_TYPES    = [ 'L', 'W' ]
-  INCLUDE_TYPES = [ 'I', 'M', 'T' ]
+  INCLUDE_TYPES = [ 'I', 'T', 'M' ]
 
   MISSING    = [ LINK_TYPES.last,  INCLUDE.last  ]
   PRESENT    = [ LINK_TYPES.first, INCLUDE.first ]
 
   def up
     Card::Reference.update_all(:present=>1)
-    Card::Reference.where(:link_type=>LINK_TYPES.last).      update_all(:present=>0, :link_type=>LINK_TYPES.first)
+    Card::Reference.where(:link_type=>LINK_TYPES.last).   update_all(:present=>0, :link_type=>LINK_TYPES.first)
     Card::Reference.where(:link_type=>INCLUDE_TYPES.last).update_all(:present=>0, :link_type=>INCLUDE_TYPES.first)
   end
 
   def down
-    Card::Reference.where(:present=>0, :link_type=>LINK_TYPES.first).      update_all(:link_type=>LINK_TYPES.last)
+    Card::Reference.where(:present=>0, :link_type=>LINK_TYPES.first).   update_all(:link_type=>LINK_TYPES.last)
     Card::Reference.where(:present=>0, :link_type=>INCLUDE_TYPES.first).update_all(:link_type=>INCLUDE_TYPES.last)
   end
 end
